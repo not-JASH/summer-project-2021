2273d573159fb700f636c32912f382db4691fd24
not-JASH <58644097+not-JASH@users.noreply.github.com>
2021-08-19 17:21:14 -0400

    added model log, started keras script

--- py-keras/network.py
+++ py-keras/network.py
@@ -1,1 +1,30 @@
 from classes import poi, pois
+from keras.models import model
+from keras.layers import Input, LSTM, Bidirectional, Dense, Activation, Dropout
+
+
+hL = 128 #hidden layers
+Ws = 360 #windowSize
+batchSize = 64
+
+model = model([
+    Input(shape=(1,Ws), batch_size = batchSize),
+
+    Bidirectional(LSTM(hL)),
+    Dropout(0.2),
+    Activation('relu'),
+    Dense(Ws),
+
+    Bidirectional(LSTM(hL)),
+    Dropout(0.2),
+    Activation('relu'),
+
+    Bidirectional(LSTM(hL)),
+    Dropout(0.2),
+    Activation('relu'),
+
+    Dense(1)
+    ])
+
+
+ 
